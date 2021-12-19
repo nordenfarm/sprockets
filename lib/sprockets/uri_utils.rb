e82da239a4a91eda60bec424f3afaa599347fd0d
Iain Donaldson <iain@sotp.xyz>
2021-12-19 22:31:00 +0000

    fixing stuff

--- lib/sprockets/uri_utils.rb
+++ lib/sprockets/uri_utils.rb
@@ -42,8 +42,7 @@ module Sprockets
     #
     # Returns [scheme, host, path, query].
     def split_file_uri(uri)
-      uri.gsub!(' ', '+')
-      scheme, _, host, _, _, path, _, query, _ = URI.split(uri)
+      scheme, _, host, _, _, path, _, query, _ = URI.split(uri.gsub(' ', '+'))
 
       path = URI::Generic::DEFAULT_PARSER.unescape(path)
       path.force_encoding(Encoding::UTF_8)
